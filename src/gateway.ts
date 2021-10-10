e4a66c2ee418931cb5925330c091cd7a65b7cd96
CHUNG QUAN TIN <chungquantin@MacBook-Pro-cua-CHUNG.local>
2021-10-10 23:30:58 +0700

    add .env.example

--- src/gateway.ts
+++ src/gateway.ts
@@ -9,9 +9,9 @@ import { register } from 'prom-client';
 import { ApolloServer } from 'apollo-server-express';
 import { MemcachedCache } from 'apollo-server-cache-memcached';
 import {
-	ApolloGateway,
-	RemoteGraphQLDataSource,
-	ServiceEndpointDefinition,
+  ApolloGateway,
+  RemoteGraphQLDataSource,
+  ServiceEndpointDefinition,
 } from '@apollo/gateway';
 import * as cors from 'cors';
 import * as fs from 'fs';
@@ -19,10 +19,10 @@ import * as express from 'express';
 import * as expressJwt from 'express-jwt';
 import * as jwt from 'jsonwebtoken';
 import {
-	AccountCreationModule,
-	BankModule,
-	LndModule,
-	TransferModule,
+  AccountCreationModule,
+  BankModule,
+  LndModule,
+  TransferModule,
 } from './modules';
 import { printSchemaWithDirectives } from 'graphql-tools';
 
@@ -30,142 +30,142 @@ import { printSchemaWithDirectives } from 'graphql-tools';
 // import { DEV_BASE_URL } from "./constants/global-variables";
 
 export const buildGateway = async () => {
-	const app = express();
+  const app = express();
 
-	const serviceList: ServiceEndpointDefinition[] = [
-		{
-			name: 'account-module',
-			url:
-				'http://localhost:3001' ||
-				(await AccountCreationModule.listen(3001)),
-		},
-		{
-			name: 'transfer-module',
-			url:
-				'http://localhost:3002' ||
-				(await TransferModule.listen(3002)),
-		},
-		{
-			name: 'bank-module',
-			url: 'http://localhost:3003' || (await BankModule.listen(3003)),
-		},
-		{
-			name: 'lnd-module',
-			url: 'http://localhost:3004' || (await LndModule.listen(3004)),
-		},
-	];
+  const serviceList: ServiceEndpointDefinition[] = [
+    {
+      name: 'account-module',
+      url:
+        'http://localhost:3001' ||
+        (await AccountCreationModule.listen(3001)),
+    },
+    {
+      name: 'transfer-module',
+      url:
+        'http://localhost:3002' ||
+        (await TransferModule.listen(3002)),
+    },
+    {
+      name: 'bank-module',
+      url: 'http://localhost:3003' || (await BankModule.listen(3003)),
+    },
+    {
+      name: 'lnd-module',
+      url: 'http://localhost:3004' || (await LndModule.listen(3004)),
+    },
+  ];
 
-	const gateway = new ApolloGateway({
-		serviceList,
-		buildService({ url }) {
-			return new RemoteGraphQLDataSource({
-				url,
-				willSendRequest({ request, context }) {
-					request.http?.headers.set(
-						'currentUser',
-						(context as any).currentUser
-							? JSON.stringify((context as any).currentUser)
-							: '',
-					);
-				},
-			});
-		},
-	});
+  const gateway = new ApolloGateway({
+    serviceList,
+    buildService({ url }) {
+      return new RemoteGraphQLDataSource({
+        url,
+        willSendRequest({ request, context }) {
+          request.http ?.headers.set(
+            'currentUser',
+            (context as any).currentUser
+              ? JSON.stringify((context as any).currentUser)
+              : '',
+          );
+        },
+      });
+    },
+  });
 
-	const { schema, executor } = await gateway.load();
+  const { schema, executor } = await gateway.load();
 
-	const sdl = printSchemaWithDirectives(schema);
-	await fs.writeFileSync(__dirname + '/schema.graphql', sdl);
+  const sdl = printSchemaWithDirectives(schema);
+  await fs.writeFileSync(__dirname + '/schema.graphql', sdl);
 
-	const corsOptions = {
-		credentials:
-			env(EnvironmentType.PROD) || env(EnvironmentType.PROD_STAGE),
-		origin: DEV_BASE_URL,
-	};
+  const corsOptions = {
+    credentials:
+      env(EnvironmentType.PROD) || env(EnvironmentType.PROD_STAGE),
+    origin: DEV_BASE_URL,
+  };
 
-	const server = new ApolloServer({
-		schema,
-		executor,
-		formatError: (err) => {
-			err.message = formatValidationError(err);
-			return err;
-		},
-		cache: new MemcachedCache(
-			[
-				'memcached-server-1',
-				'memcached-server-2',
-				'memcached-server-3',
-			],
-			{ retries: 10, retry: 10000 }, // Options
-		),
-		context: ({ req }): Partial<GQLContext> => {
-			const token =
-				req.body.token ||
-				req.query.token ||
-				req.headers['x-access-token'];
+  const server = new ApolloServer({
+    schema,
+    executor,
+    formatError: (err) => {
+      err.message = formatValidationError(err);
+      return err;
+    },
+    cache: new MemcachedCache(
+      [
+        'memcached-server-1',
+        'memcached-server-2',
+        'memcached-server-3',
+      ],
+      { retries: 10, retry: 10000 }, // Options
+    ),
+    context: ({ req }): Partial<GQLContext> => {
+      const token =
+        req.body.token ||
+        req.query.token ||
+        req.headers['x-access-token'];
 
-			try {
-				if (token !== '') {
-					const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-					(req as any).user = decoded;
-				}
-				return {
-					request: req,
-					currentUser: (req as any).user || undefined,
-					redis: new REDIS().server,
-					url: req?.protocol + '://' + req?.get('host'),
-				};
-			} catch (error) {
-				return {
-					request: req,
-					currentUser: undefined,
-					redis: new REDIS().server,
-					url: req?.protocol + '://' + req?.get('host'),
-				};
-			}
-		},
-	});
+      try {
+        if (token !== '') {
+          const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+          (req as any).user = decoded;
+        }
+        return {
+          request: req,
+          currentUser: (req as any).user || undefined,
+          redis: new REDIS().server,
+          url: req ?.protocol + '://' + req ?.get('host'),
+        };
+      } catch (error) {
+        return {
+          request: req,
+          currentUser: undefined,
+          redis: new REDIS().server,
+          url: req ?.protocol + '://' + req ?.get('host'),
+        };
+      }
+    },
+  });
 
-	await server.start();
+  await server.start();
 
-	server.applyMiddleware({ app: app as any });
+  server.applyMiddleware({ app: app as any });
 
-	app.use(
-		expressJwt({
-			secret: 'f1BtnWgD3VKY',
-			algorithms: ['HMACSHA256'],
-			credentialsRequired: false,
-		}),
-	);
-	app.use(cors(corsOptions));
-	app.use(express.json());
-	app.use(express.urlencoded({ extended: true }));
+  app.use(
+    expressJwt({
+      secret: 'f1BtnWgD3VKY',
+      algorithms: ['HMACSHA256'],
+      credentialsRequired: false,
+    }),
+  );
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-	app.use('*', (req, _, next) => {
-		const query = req.query.query || req.body.query || '';
-		if (query.length > 2000) {
-			throw new Error('Query too large');
-		}
-		next();
-	});
+  app.use('*', (req, _, next) => {
+    const query = req.query.query || req.body.query || '';
+    if (query.length > 2000) {
+      throw new Error('Query too large');
+    }
+    next();
+  });
 
-	// Grafana Configuration
-	app.use('/metric', async (req, res) => {
-		try {
-			res.set('Content-Type', register.contentType);
-			res.end(await register.metrics());
-		} catch (err) {
-			res.status(500).end(err);
-		}
-	});
+  // Grafana Configuration
+  app.use('/metric', async (req, res) => {
+    try {
+      res.set('Content-Type', register.contentType);
+      res.end(await register.metrics());
+    } catch (err) {
+      res.status(500).end(err);
+    }
+  });
 
-	const PORT = env(EnvironmentType.TEST)
-		? 8080
-		: process.env.PORT || 3000;
+  const PORT = env(EnvironmentType.TEST)
+    ? 8080
+    : process.env.PORT || 3000;
 
-	await app.listen({ port: PORT });
+  await app.listen({ port: PORT });
 
-	console.log(
-		`Gateway is ready at ${process.env.SERVER_URI}:${PORT}${process.env.SERVER_ENDPOINT}`,
-	);
+  console.log(
+    `Gateway is ready at ${process.env.SERVER_URI}:${PORT}${process.env.SERVER_ENDPOINT}`,
+  );
 };
